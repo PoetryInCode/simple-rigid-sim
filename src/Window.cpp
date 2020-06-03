8300f187618243b347cd469503db16c075f8cbaa
SoloArchx250 <SolomonWagner@protonmail.com>
2020-06-03 17:15:04 -0400

    formatting and gitignore

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,17 +1,17 @@
 #include "Window.h"
 
-int Window::setClearColor(Color c) {
+void Window::setClearColor(Color c) {
   clearColor = &c;
 }
 
-int Window::setClearColor(uint8_t r,Uint8 g,Uint8 b,Uint8 a) {
+void Window::setClearColor(uint8_t r,Uint8 g,Uint8 b,Uint8 a) {
   clearColor[0].r = r;
   clearColor[0].g = g;
   clearColor[0].b = b;
   clearColor[0].a = a;
 }
 
-int Window::initUI() {
+void Window::initUI() {
   if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     fprintf(stderr,"\033[92m[ ERROR ] Could not initialize SDL:\n%s\n", SDL_GetError());
   }
--- src/Window.h
+++ src/Window.h
@@ -7,8 +7,8 @@ class Window {
   Color *clearColor = new Color(BLACK);
   SDL_Window *win;
   SDL_Renderer *rend;
-  virtual int update() {};
-  int initUI() {};
-  int setClearColor(Color c) {};
-  int setClearColor(Uint8 r,Uint8 g,Uint8 b,Uint8 a) {};
+  virtual int update();
+  void initUI();
+  void setClearColor(Color c);
+  void setClearColor(Uint8 r,Uint8 g,Uint8 b,Uint8 a);
 };
